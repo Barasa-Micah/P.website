b1c77a8d0c04246b5542eb856fc590d125049975
Barasa-Micah <barasamica@gmail.com>
2023-06-12 20:46:01 +0300

    add changes in the html and css

--- script.js
+++
@@ -1,7 +0,0 @@
-var typed = new typed (".text",{
-    string:["Frontend Developer","Web Developer","Software Engineer"],
-    typespeed:100,
-    backspeed:100,
-    backdelay:1000,
-    loop:true,
-})
